338812fd7cf570f67e6ea0b9a9b7580397003975
Moch Fikri Setiawan <mixchannel1307@gmail.com>
2024-04-01 10:39:09 +0700

    pertemuan 7 - praktikum 6

--- pertemuan7js7/form_ajax.php
+++ pertemuan7js7/form_ajax.php
@@ -1,57 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Contoh Form dengan PHP dan jQuery</title>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-</head>
-<body>
-    <h2>Form Contoh</h2>
-    <form id="myForm">
-        <label for="buah">Pilih Buah : </label>
-        <select name="buah" id="buah">
-            <option value="apel">Apel</option>
-            <option value="pisang">Pisang</option>
-            <option value="mangga">Mangga</option>
-            <option value="jeruk">Jeruk</option>
-        </select>
+<html>
+    <head>
+        <title>Contoh Form dengan PHP dan JQuery</title>
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    </head>
+    <body>
+        <h2>Form Contoh</h2>
+        <form id="myForm">
+            <label for="buah">Pilih Buah</label>
+            <select name="buah" id="buah">
+                <option value="apel">Apel</option>
+                <option value="pisang">Pisang</option>
+                <option value="mangga">Mangga</option>
+                <option value="jeruk">Jeruk</option>
+            </select>
 
-        <br>
+            <br>
 
-        <label>Pilih Warna Favorit</label><br>
-        <input type="checkbox" name="warna[]" value="merah">Merah <br>
-        <input type="checkbox" name="warna[]" value="biru">Biru <br>
-        <input type="checkbox" name="warna[]" value="hijau">Hijau
+            <label>Pilih Warna Favorit</label><br>
+            <input type="checkbox" name="warna[]" value="merah"> Merah<br>
+            <input type="checkbox" name="warna[]" value="biru"> Biru<br>
+            <input type="checkbox" name="warna[]" value="hijau"> Hijau<br>
 
-        <br>
+            <br>
 
-        <label>Pilih Jenis Kelamin</label><br>
-        <input type="radio" name="jenis_kelamin" value="laki-laki"> Laki-laki <br>
-        <input type="radio" name="jenis_kelamin" value="perempuan"> Perempuan 
+            <label>Pilih Jenis Kelamin</label><br>
+            <input type="radio" name="jenis_kelamin" value="laki-laki"> Laki-laki<br>
+            <input type="radio" name="jenis_kelamin" value="perempuan"> Perempuan<br>
 
-        <br>
+            <br>
 
-        <input type="submit" value="Submit">
-    </form>
+            <input type="submit" value="submit">
+        </form>
 
-    <div id="hasil"></div>
+        <div id="hasil">
 
-    <script>
-        $(document).ready(function(){
-            $("#myForm").submit(function(e){
-                e.preventDefault();
+        </div>
 
-                var formData = $("#myForm").serialize();
-
-                $ajax({
-                    url: "proses_lanjut.php",
-                    Type: "POST",
-                    data: formData,
-                    succes: function(response){
-                        $("#hasil").html(response);
-                    }
-                })
-            })
-        })
-    </script>
-</body>
+        <script>
+            $(document).ready(function() {
+                $("#myForm").submit(function (e) {
+                    e.preventDefault();
+                    var formData = $("#myForm").serialize();
+                    $.ajax({
+                        url: "proses_lanjut.php",
+                        type: "post",
+                        data: formData,
+                        success: function (response) {
+                            $("#hasil").html(response);
+                        }
+                    });
+                });
+            });
+        </script>
+    </body>
 </html>
