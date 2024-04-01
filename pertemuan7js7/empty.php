ceddeef5221a7caad2de32ec6570c2ceb942d5bb
Moch Fikri Setiawan <mixchannel1307@gmail.com>
2024-04-01 09:19:46 +0700

    pertemuan 7 - praktikum 2

---
+++ pertemuan7js7/empty.php
@@ -0,0 +1,8 @@
+<?php
+    $myArray = array();
+    if(empty($nonExistentVar)) {
+        echo "Array tidak terdefinisi atau kosong";
+    } else {
+        echo "Array terdefenisi dan tidak kosong";
+    }
+?>
